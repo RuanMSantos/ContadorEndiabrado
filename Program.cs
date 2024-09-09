21977e9a9aee77363af9afb4e6542e5e1228a041
RuanMSantos <ruancauancauan@gmail.com>
2024-09-09 17:58:13 -0300

    Finaliza programa

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Figgle;
+
+const int limiteContagem = 100;
+int contador = 1;
+
+Contador();
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(FiggleFonts.Big.Render("Explosão monstra 2.0!"));
+Console.ResetColor();
+
+void Contador()
+{
+    #pragma warning disable CA1416 
+    Thread.Sleep(101 - contador);
+    Console.Write($"{contador} ");
+    Console.Beep(5000, 101 - contador);
+    contador++;
+    #pragma warning restore CA1416
+    if (contador <= limiteContagem)
+    {
+        Contador();
+    }
+}
